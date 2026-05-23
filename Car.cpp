45326c8f582fda90c07a8bf3620e5405c315874a
Shivam <tripathishivam9026@gmail.com>
2026-05-23 22:07:23 +0530

    added Car

---
+++ Car.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+class Car{
+    public:
+    string name;
+    int price;
+    int speed;
+    int seat;
+};
+int main(){
+    Car c1;
+    c1.name="honda City";
+    c1.price=450000;
+    c1.seat=5;
+    c1.speed=280;
+
+    Car c2;
+    c2.name="swift";
+    c2.price=10000;
+    c2.seat=4;
+    c2.speed=180;
+        Car c3;
+    c3.name="defender";
+    c3.speed=500;
+    c3.price=500000;
+    c3.seat=8;
+
+    cout<<c3.name<<" "<<c3.speed<<" " <<
+c3.seat<<" "<<c3.price;
+}
